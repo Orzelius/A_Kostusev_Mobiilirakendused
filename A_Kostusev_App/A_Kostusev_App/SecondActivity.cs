4063461a3c9bd2cadc9f60d05440b4e9d314fe54
Orzelius <albertkostusev@gmail.com>
2019-09-11 21:49:24 +0300

    Sync with desktop

--- A_Kostusev_App/A_Kostusev_App/SecondActivity.cs
+++ A_Kostusev_App/A_Kostusev_App/SecondActivity.cs
@@ -9,15 +9,42 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Data;
 
 namespace A_Kostusev_App {
     [Activity(Label = "SecondActivity")]
     public class SecondActivity : Activity {
+
+        DataTable dt;
+        EditText editText;
+        TextView textView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            dt = new DataTable();
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.second_layout);
             // Create your application here
+            textView = FindViewById<TextView>(Resource.Id.textView1);
+            editText = FindViewById<EditText>(Resource.Id.editText1);
+
+            editText.TextChanged += EditText_TextChanged;
+        }
+
+        private void EditText_TextChanged(object sender, Android.Text.TextChangedEventArgs e) {
+            try {
+                int solution = Convert.ToInt32(dt.Compute(editText.Text.ToString(), "").ToString());
+            }
+            catch(Exception ex) {
+                editText.Text = "Err";
+            }
+        }
+
+        protected override void Dispose(bool disposing) 
+        {
+            base.Dispose(disposing);
+
+            dt.Dispose();
         }
     }
 }
