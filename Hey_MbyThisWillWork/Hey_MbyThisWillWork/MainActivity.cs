bfedb6ca12e614fe68caad795e84e1da15212f49
Orzelius <albertkostusev@gmail.com>
2019-09-12 16:50:07 +0300

    Sync with laptop

--- Hey_MbyThisWillWork/Hey_MbyThisWillWork/MainActivity.cs
+++ Hey_MbyThisWillWork/Hey_MbyThisWillWork/MainActivity.cs
@@ -24,13 +24,38 @@ namespace Hey_MbyThisWillWork
 
             calculatorButton = (Button)FindViewById(Resource.Id.calculator);
 
+            
             colorButtons[0] = (Button)FindViewById(Resource.Id.activities_button0);
             colorButtons[1] = (Button)FindViewById(Resource.Id.activities_button1);
             colorButtons[2] = (Button)FindViewById(Resource.Id.activities_button2);
             colorButtons[3] = (Button)FindViewById(Resource.Id.activities_button3);
 
 
+            colorButtons[0].Click += ColorButtonClick0;
+            colorButtons[1].Click += ColorButtonClick1;
+            colorButtons[2].Click += ColorButtonClick2;
+            colorButtons[3].Click += ColorButtonClick3;
             calculatorButton.Click += CalculatorButton_Click;
+        }
+
+        private void ColorButtonClick3(object sender, EventArgs e)
+        {
+            //StartActivity(typeof());
+        }
+
+        private void ColorButtonClick2(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ColorButtonClick1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ColorButtonClick0(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void CalculatorButton_Click(object sender, EventArgs e)
